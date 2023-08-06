291d3b7a9140b92f58ad462e99e58d65f15a9deb
Neffaris <122637989+Neffaris@users.noreply.github.com>
2023-08-06 11:36:12 +0200

    Add files via upload

--- eva_tasks.py
+++ eva_tasks.py
@@ -1,13 +1,32 @@
 import customtkinter as ctk 
 
 
+labels_and_buttons = []
 
-def add_task_todo():
+def delete_task(index):
+    if len(labels_and_buttons) > index:
+        label, button = labels_and_buttons[index]
+        label.grid_remove()
+        button.grid_remove()
+
+    #labels_and_buttons.pop(index)
+
+def add_task(labels_and_buttons):
     todo = entry.get()
-    label = ctk.CTkLabel(scrollable_frame, text=todo)
-    label.pack()
+    if todo == "":
+        pass
+    else:
+        global label
 
-    entry.delete(0, ctk.END)
+        label = ctk.CTkLabel(scrollable_frame, text=todo)
+        label.grid()
+
+        delete_task_button = ctk.CTkButton(scrollable_frame, text="Remove task", command=lambda idx=len(labels_and_buttons): delete_task(idx))
+        delete_task_button.grid()
+
+        labels_and_buttons.append((label, delete_task_button))
+
+        entry.delete(0, ctk.END)
 
 def eva_tasks():
     root = ctk.CTk()
@@ -15,19 +34,18 @@ def eva_tasks():
     root.title("EVA Tasks")
     
     title_label = ctk.CTkLabel(root, text="Tasks List", font=ctk.CTkFont(size=30, weight="bold"))
-    title_label.pack(padx=10, pady=(40, 20))
+    title_label.grid(padx=275, pady=(40, 20))
 
     global scrollable_frame
     scrollable_frame = ctk.CTkScrollableFrame(root, width=500, height=100)
-    scrollable_frame.pack()
+    scrollable_frame.grid()
 
     global entry
     entry = ctk.CTkEntry(scrollable_frame, placeholder_text="Add task todo")
-    entry.pack(fill="x")
+    entry.grid(padx=175)
+    #entry.grid(fill="x")
 
-    add_button = ctk.CTkButton(root, text="Add task", width=500, height=50, command=add_task_todo)
-    add_button.pack(pady=20)
+    add_button = ctk.CTkButton(root, text="Add task", width=500, height=50, command=lambda: add_task(labels_and_buttons))
+    add_button.grid(pady=20)
 
     root.mainloop()
-
-eva_tasks()
